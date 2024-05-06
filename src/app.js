faa9240988cc355355765756bd6cf98f814bc98a
Marci25 <126285355+Marci25@users.noreply.github.com>
2024-05-06 19:28:33 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  documen;
+  console.log();
+};
+let generateExcuse = () => {
+  let pronoun = ["A ", "The "];
+  let subject = ["son ", "coworker  ", "dog ", "cleaner ", "assitant "];
+  let action = ["took my ", "threw my ", "stole my ", "broke my"];
+  let possetion = ["homework ", "toe ", "car ", "shoe "];
+  let where = ["on the street", "in my house ", "in my driveway "];
+
+  let pronounIndex = Math.random(Math.floor() * pronoun.length);
+  let subjectIndex = Math.random(Math.floor() * subject.length);
+  let actionIndex = Math.random(Math.floor() * action.length);
+  let possetionIndex = Math.random(Math.floor() * possetion.length);
+  let whereIndex = Math.random(Math.floor() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
